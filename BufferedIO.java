9168b2e3fdfcab45a9fc10244984f8d8f81054fd
Devendra Kumar <dk1721080@gmail.com>
2024-04-01 23:49:47 +0530

    new java file create

---
+++ BufferedIO.java
@@ -0,0 +1,42 @@
+package inputoutput;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class BufferedIO {
+
+    public static void main(String[] args) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\Shiley\\Desktop\\myfile.txt", false);
+
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+            String s = "My name is Amit Kumar";
+            byte b[] = s.getBytes();
+
+            Timer.startTime();
+            bufferedOutputStream.write(b);
+            bufferedOutputStream.flush(); // Make sure all buffered data is written to the file
+            Timer.endTime();
+
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(
+                    new FileInputStream("C:\\Users\\Shiley\\Desktop\\myfile.txt"));
+            while (true) {
+                int i = bufferedInputStream.read();
+                if (i == -1) {
+                    break;
+                }
+                System.out.print((char) i);
+            }
+            // Closing streams
+            bufferedOutputStream.close();
+            bufferedInputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
