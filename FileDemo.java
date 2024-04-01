4734bf2bf9e2aa02d3f86ea8ef61fdeb3302535b
Devendra Kumar <dk1721080@gmail.com>
2024-04-01 21:39:53 +0530

    some changes

--- FileDemo.java
+++ FileDemo.java
@@ -3,13 +3,16 @@ package inputoutput;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
+import javax.swing.JOptionPane;
+
 public class FileDemo {
 
     @SuppressWarnings("resource")
     public static void main(String[] args) {
         try {
             FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\Shiley\\Desktop\\myfile.txt", false);
-            String s = "My name is Devendra Kumar";
+            // String s = "My name is Devendra Kumar";
+            String s = JOptionPane.showInputDialog(null, "Enter your text..");// user input
             byte b[] = s.getBytes();// convert string into byte
             for (int i = 0; i < b.length; i++) {
                 System.out.print(b[i] + " "); // print character unicode value
@@ -21,7 +24,7 @@ public class FileDemo {
             FileInputStream fileInputStream = new FileInputStream("C:\\Users\\Shiley\\Desktop\\myfile.txt");
 
             while (true) {
-                int i = fileInputStream.read();//this method return integer value
+                int i = fileInputStream.read();// this method return integer value
                 if (i == -1) {
                     break;
                 }
